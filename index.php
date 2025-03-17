b796e5ed3ab6097665165b6a656e6accbbfe002a
yzhaaaa <johnhenrybarrameda@gmail.com>
2025-03-17 21:34:30 +0800

    Updated index.php, completed_task.php (added favicon)

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" href="images/checklist.png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
